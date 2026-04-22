e85e44db886fba3060808850ab4abdc3d3160e0c
Mark Raspopov <markraspopov0@gmail.com>
2026-04-22 15:00:09 -0700

    made client send information about what frame it wants and able to pause, skip, and change speed

--- host_video.py
+++ host_video.py
@@ -2,14 +2,20 @@ import cv2
 import socket
 import pickle
 import sys
-import time
+import json
 
-def main(host: str, port: int):
+def main(host_ip, host_port, client_ip, client_port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((host_ip, host_port))
+
     cap = cv2.VideoCapture("input_video.mp4")
     fps = int(cap.get(cv2.CAP_PROP_FPS)) or 30
 
     while True:
+        data, _ = sock.recvfrom(1024)
+        data = json.loads(data.decode('utf-8'))
+
+        cap.set(cv2.CAP_PROP_POS_FRAMES, data["frame"])
         ok, frame = cap.read()
         if not ok:
             break
@@ -19,11 +25,9 @@ def main(host: str, port: int):
             continue
 
         payload = pickle.dumps(encoded)
-        sock.sendto(payload, (host, port))
-
-        time.sleep(1/fps)
+        sock.sendto(payload, (client_ip, client_port))
 
     cap.release()
 
 if __name__ == "__main__":
-    main(sys.argv[1], int(sys.argv[2]))
+    main(sys.argv[1], int(sys.argv[2]), sys.argv[3], int(sys.argv[4]))
--- play_video.py
+++ play_video.py
@@ -2,12 +2,21 @@ import cv2
 import pickle
 import socket
 import sys
+import json
 
-def main(bind_ip: str, port: int) -> None:
+def main(host_ip, host_port, client_ip, client_port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind((bind_ip, port))
+    sock.bind((client_ip, client_port))
+
+    i = 0
+    speed = 1
+    paused = False
 
     while True:
+        request = {"frame": int(i)}
+        json_bytes = json.dumps(request).encode('utf-8')
+        sock.sendto(json_bytes, (host_ip, host_port))
+
         data, _ = sock.recvfrom(65536)
         encoded = pickle.loads(data)
         frame = cv2.imdecode(encoded, cv2.IMREAD_COLOR)
@@ -16,10 +25,27 @@ def main(bind_ip: str, port: int) -> None:
             continue
 
         cv2.imshow("Frame", frame)
-        if cv2.waitKey(1) & 0xFF == ord("q"):
+
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
             break
+        if key == ord("k"):
+            paused = not paused
+        elif key == ord("j"):
+            i -= 150
+        elif key == ord("l"):
+            i += 150
+        elif key == ord("i"):
+            speed = 2
+        elif key == ord("m"):
+            speed = 0.5
+        elif key == ord(";"):
+            speed = 1
+
+        if not paused:
+            i += speed
 
     cv2.destroyAllWindows()
 
 if __name__ == "__main__":
-    main(sys.argv[1], int(sys.argv[2]))
+    main(sys.argv[1], int(sys.argv[2]), sys.argv[3], int(sys.argv[4]))
